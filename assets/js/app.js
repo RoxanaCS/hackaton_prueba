4ac044f039f7229b9f74064ee72103c6fcc6a278
RoxanaCS <roxale.cares@gmail.com>
2018-01-10 13:46:59 -0300

    utilización de la api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
-})
+});
+//Al hacer click en el boton de registro con google:
+document.getElementById('btnsignUp').addEventListener('click',GoogleSignUp, false);
+//ocultar segunda section
+$('.search-section').hide();
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyCdK7laKVDh6CC4E9yNT_YEHu1wL5JO5-8",
@@ -11,17 +15,16 @@ var config = {
 };
 firebase.initializeApp(config);
 
-//función de ingreso con googleapis
-
+//función de ingreso con google
 function GoogleSignUp(){
-//  if (!firebase.auth().currentUser){  //para saber si el usuario se ha conectado
+  if (!firebase.auth().currentUser){  //para saber si el usuario se ha conectado
     var provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
     firebase.auth().signInWithPopup(provider).then(function(result) {
-        console.log(provider);
       var token = result.credential.accessToken;
       var user = result.user;
-      console.log(user);
+      $('.inicio').hide() && $('.search-section').show();
+    //  console.log(user);
     }).catch(function(error){
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -32,8 +35,26 @@ function GoogleSignUp(){
         alert('Es el mismo usuario');
       }
     });
-  //}else{
-  //  firebase.auth().signOut();
-  //}
+  }else{
+    firebase.auth().signOut();
+  }
 }
-document.getElementById('btnsignUp').addEventListener('click',GoogleSignUp, false);
+//función para usar la API de imbd
+function apiCall(movie){
+	$.getJSON('http://www.omdbapi.com/?apikey=3a181f1c&t=' + encodeURI(movie)).then(function(response){
+    if(response.Title != undefined){
+        $('.foto').html(''); //limpiamos el contenedor
+        //dentro de acá debo sacar todos los objetos
+        $('.foto').append(
+          '<img src=' + response.Poster + '>' +
+          '<p>' + response.imdbRating +'</p>'
+          )
+      }
+    })
+}
+//hago click en el buscador de peliculas
+  $('#submit-movie').click(function(){
+    var movieSearch = $('#busqueda').val();
+    apiCall(movieSearch);
+    //console.log(apiCall(movieSearch));
+  });
